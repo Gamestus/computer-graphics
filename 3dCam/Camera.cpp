ce1d1c9654eaa292b467a7434e8be221ea703f39
Gamestus <60428049+Gamestus@users.noreply.github.com>
2024-03-06 14:48:56 +0300

    Switching modes for cam

--- 3dCam/Camera.cpp
+++ 3dCam/Camera.cpp
@@ -3,20 +3,26 @@
 
 Camera::Camera()
 {
-	globalPosition.z = distance;
 }
 
 DirectX::XMMATRIX Camera::GetMatrix()
 {
-    auto pos = dx::XMVector3Transform(
-        dx::XMVectorSet(globalPosition.x, globalPosition.y, globalPosition.z, 0.0f),
-        dx::XMMatrixRotationRollPitchYaw(orbitPhi, -orbitTheta, 0.0f)
-    );
-    auto matrix = dx::XMMatrixLookAtLH(
-        pos,
-        dx::XMVectorZero(),
-        dx::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)
-    ) * dx::XMMatrixRotationRollPitchYaw(rotation.x, rotation.y, rotation.z);
+	DirectX::XMMATRIX matrix;
+	if (isOrbit) {
+		auto pos = dx::XMVector3Transform(
+			dx::XMVectorSet(0, 0, -distance, 0.0f),
+			dx::XMMatrixRotationRollPitchYaw(orbitPhi, -orbitTheta, 0.0f)
+		);
+		matrix = dx::XMMatrixLookAtLH(
+			pos,
+			dx::XMVectorZero(),
+			dx::XMVectorSet(0.0f, 1.0f, 0.0f, 0.0f)
+		) * dx::XMMatrixRotationRollPitchYaw(rotation.x, rotation.y, rotation.z);
+	}
+	else {
+		matrix = dx::XMMatrixTranslation(globalPosition.x, globalPosition.y, globalPosition.z);
+	}
+
     return matrix * dx::XMMatrixPerspectiveLH(1.0f, 1.0f, 0.4f, 10.0f);
 }
 
@@ -25,10 +31,10 @@ DirectX::XMMATRIX Camera::GetMatrix()
 void Camera::Update(float delta) {
 	Vector2 input;
 
-	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::Up))) {
+	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::Down))) {
 		input.y = 1;
 	}
-	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::Down))) {
+	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::Up))) {
 		input.y = -1;
 	}
 
@@ -47,28 +53,47 @@ void Camera::Update(float delta) {
 
 	orbitPhi = clampedValue;
 	orbitTheta -= input.x * delta * 2;
+	if (!isOrbit)
+	{
+		rotation.x = -clampedValue;
+		rotation.y += input.x * delta * 2;
+	}
+
 
 	Vector3 moveInput;
-	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::E))) {
-		moveInput.z = 1;
-	}
-	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::Q))) {
-		moveInput.z = -1;
-	}
-
-	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::W))) {
-		moveInput.x = 1;
-	}
-	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::S))) {
-		moveInput.x = -1;
-	}
-
-	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::A))) {
+	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::Q))) {
 		moveInput.y = 1;
 	}
-	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::D))) {
+	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::E))) {
 		moveInput.y = -1;
 	}
 
-	rotation += moveInput * delta * 2;
+	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::S))) {
+		moveInput.z = 1;
+	}
+	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::W))) {
+		moveInput.z = -1;
+	}
+
+	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::A))) {
+		moveInput.x = 1;
+	}
+	else if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::D))) {
+		moveInput.x = -1;
+	}
+
+	globalPosition += moveInput * delta * 2;
+	clampedValue = distance + moveInput.z * delta * 2;
+	if (clampedValue < 0.2)
+		clampedValue = 0.2;
+	else if (clampedValue > 12.5f)
+		clampedValue = 12.5f;
+	distance = clampedValue;
+
+	if (Game::Instance->InDevice->IsKeyDown(static_cast<u_char>(Keys::Tab))) {
+		rotation = Vector3(0, 0, 0);
+		globalPosition = Vector3(0, 0, distance);
+		distance = 5.0f;
+		isOrbit = !isOrbit;
+	}
 }
--- 3dCam/Camera.h
+++ 3dCam/Camera.h
@@ -9,6 +9,7 @@ public:
     dx::XMMATRIX GetMatrix();
 	void Update(float delta);
 private:
+    bool isOrbit = true;
     float distance = 5.0f;
     float orbitTheta = 0.0f;
     float orbitPhi = 0.0f;
